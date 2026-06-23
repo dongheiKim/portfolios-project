f195a04b8cf116600a87b754835e8f367d7dc976
kimdonghei <anytime1994@gmail.com>
2026-06-23 17:29:14 +0900

    feat: approuter 헤더 및 푸터 컴포넌트 추가

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -4,11 +4,22 @@ import { ProductDetailPage } from "@/pages/product-detail";
 import { SignupPage } from "@/pages/signup";
 import { LoginPage } from "@/pages/login";
 import { NotFoundPage } from "@/pages/not-found";
+import { Header } from "@/widgets/header";
+import { Footer } from "@/widgets/footer";
+import "@/app/styles/global.css";
 
 export const AppRouters = [
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/",
+    element: <Header />,
+  },
+  {
+    path: "/",
+    element: <Footer />,
   },
   {
     path: "/orders",
