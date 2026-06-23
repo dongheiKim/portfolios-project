aa6abddc61f25fefb8652e696fb7226daa48b125
kimdonghei <anytime1994@gmail.com>
2026-06-23 16:28:59 +0900

    feat: 추가된 페이지 route 구성

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,8 +1,33 @@
 import { HomePage } from "@/pages/home";
+import { OrdersPage } from "@/pages/orders";
+import { ProductDetailPage } from "@/pages/product-detail";
+import { SignupPage } from "@/pages/signup";
+import { LoginPage } from "@/pages/login";
+import { NotFoundPage } from "@/pages/not-found";
 
 export const AppRouters = [
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/orders",
+    element: <OrdersPage />,
+  },
+  {
+    path: "/product-detail",
+    element: <ProductDetailPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
   },
 ];
