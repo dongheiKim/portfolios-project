2bbccabdeba0377a19c3e21cd51f3ad2b03d9169
kimdonghei <anytime1994@gmail.com>
2026-06-24 20:23:38 +0900

    feat: ErrorBoundary - 컴포넌트 추가

---
+++ src/app/error-boundary/ErrorBoundary.tsx
@@ -0,0 +1,29 @@
+import { Component, type ErrorInfo, type ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  fallback?: ReactNode;
+}
+
+export class ErrorBoundary extends Component<Props, { hasError: boolean }> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: Error): { hasError: boolean } {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback || <h1>오류가 발생하였습니다.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
