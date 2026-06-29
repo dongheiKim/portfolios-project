4b12facd0b1237a9aa96f789f6bdee80bf15d968
kimdonghei <anytime1994@gmail.com>
2026-06-29 23:46:34 +0900

    feat: header - 링크 추가

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router";
+
 export function Header() {
   return (
     <header className="bg-white border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
-        <div className="text-xl font-bold text-gray-800">쿠팡</div>
+        <div className="text-xl font-bold text-gray-800">
+          <Link
+            to="/"
+            className="flex-shrink-0  text-white font-black text-2xl tracking-tight"
+          >
+            쿠팡
+          </Link>
+        </div>
         <nav className="space-x-4">
           <a href="/" className="text-gray-600 hover:text-gray-800">
             Home
